f92898ed21c8c9b403ad8221ab340a321205e976
Iannarelli <lipe.iannarelli@gmail.com>
2022-08-02 16:00:04 -0300

    layouts

--- resources/views/first_page.blade.php
+++ resources/views/first_page.blade.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <p>Oi, {{$name}}! Você está na primeira página.</p>
     <a href="/second_page">Link</a>
-</body>
-</html>
+
+    @stop
+
+@section('footer')
+    <script>alert('Olá!')</script>
+
+    @stop
